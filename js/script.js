32bda09d9590b20fe2572539f59f96b2afaec721
jonyrahman626 <jonyrahman15@gmail.com>
2022-04-29 20:49:58 +0400

    litening page

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(function () {
+    'use strict';
+
+    // Lightning slider start
+
+    $('.light_project_slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+
+
+
+})
